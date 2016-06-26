b32fbb7b8717d2957dfe547a93330b9b1af06fcb
Nikolai <prosto2307@gmail.com>
2016-06-26 17:47:51 +0300

    migration script changes

--- migration.py
+++ migration.py
@@ -1,11 +1,11 @@
 import csv
 import os
 from collections import defaultdict
-#from pymongo import MongoClient
+from pymongo import MongoClient
 import json
 
-import re
-#db = MongoClient(os.environ.get('MONGODB_URI')).heroku_rcbs36lq
+db = MongoClient(os.environ.get('MONGODB_URI')).heroku_rcbs36lq
+
 lang_list = ['Англійська мова', 'Французька мова', 'Німецька мова','Іспанська мова', 'Російська мова']
 useless_zno = ['Психологія','Правознавство','Екологія','Економіка','Інформатика','Людина і світ']
 zno_dict = {
@@ -49,13 +49,16 @@ fach_tvorch = ['tvorch_1','tvorch_2','tvorch_3',
                 'fach_test_3','fach_test_4','fach_test_5',
                 'fach_test_6','fach_test_7','fach_test_8','fach_test_9']
 
+def replace_nans(name):
+    if name == '': return 'Дані відсутні'
+    else: return name
+
 counter = 21576
 import glob
-files = glob.glob("C:/Users/Lina/Dropbox/myvstup/competitions/*.csv")
+files = glob.glob("C:/Users/prost/Dropbox/myvstup/competitions/*.csv")
 
-data = []
+full_data = []
 for f in files:
-
 
     with open(f,'r',encoding = 'utf-8') as file:
         reader = csv.reader(file)
@@ -64,10 +67,10 @@ for f in files:
             if len(row)!=0:
                 data_row = defaultdict(dict)
                 data_row['cityName'] = str(f.split('\\')[-1].split('.')[0])
-                data_row['universityName'] = str(row[0])
-                data_row['facultatyName'] = str(row[1])
+                data_row['universityName'] = replace_nans(str(row[0]))
+                data_row['facultatyName'] = replace_nans(str(row[1]))
                 data_row['specialityId'] = str(row[2])
-                data_row['specialityName'] = str(row[3])
+                data_row['specialityName'] = replace_nans(str(row[3]))
                 data_row['placeRange'] = {"all":int(row[4]),
                                           "free":int(row[5]),
                                           "payable":int(row[6])}
@@ -84,80 +87,47 @@ for f in files:
                 coefs_dict = {}
                 
                 temp_dict = json.loads(row[9].replace("'",'"'))
-                #print([type(i) for i in list(temp_dict.values())])
-                #print(temp_dict.values())
-                
-                obligative_zno=[]
-                optional_zno=[]
-                optional_zno_coef=0
-                for x in  list(temp_dict.keys()):
 
-                    if type(temp_dict[x])==str:
-                        if "мова" in x:
-                            print('')
-                        else:
-                            optional_zno.append(x)
-                        #optional_zno_coef=float(re.findall("[-+]?\d+[\.]?\d*", temp_dict[x])[0])
-                        optional_zno_coef, exam = temp_dict[x].split(') або ')
-                        optional_zno.append(exam)
-                    else:
-                        if temp_dict[x]!=0:   
-                            obligative_zno.append(x)                
-                
-               
-                #print(obligative_zno)
-                '''for i in optional_zno:
-                    if i!=[]:
-                        print(obligative_zno)
-                        print(optional_zno)
-                        print(optional_zno_coef)'''
-                      
-                
+                obligative_zno = []
+                extra_zno_list = []
                 if str in [type(i) for i in list(temp_dict.values())]:
                 
                     str_indexes = [j for j,i in enumerate([type(i) for i in list(temp_dict.values())]) if i==str]
-                    #print (str_indexes)                    
-                    extra_zno_list=[]
                     for str_item in str_indexes:
                         str_value = temp_dict[list(temp_dict.keys())[str_item]]
-                        #print(temp_dict[list(temp_dict.keys())[str_item]])
                         coef, extra_zno = str_value.split(') або ')
-                        #print(type(extra_zno))
                         temp_dict[list(temp_dict.keys())[str_item]] = float(coef)
-                        #print(list(temp_dict.keys())[str_item])
                         temp_dict[extra_zno] = float(coef)
-                        extra_zno_list.append(extra_zno)
-                        #print(list(temp_dict.keys())[str_item])
-                        #print(temp_dict[list(temp_dict.keys())[str_item]])
-                        
-                    #print(extra_zno_list)
-                    
+
+                        if 'Іноземна мова (' in key:
+                            splited_names = key.split('(')[-1].split(')')[0].split(', ')
+                        extra_zno_list += splited_names + [extra_zno]  
+
                 non_zero_keys = [k for k in list(temp_dict.keys()) if temp_dict[k]!=0]
                 for key in non_zero_keys:
                     try:
                         if key == 'Іноземна мова':
                             coefs_dict.update({lang: float(temp_dict[key]) for lang in zno_dict[key]})
-                            optional_zno += lang_list
+                            extra_zno_list += lang_list
                         else:
                             db_name = zno_dict[key]
                             coefs_dict[db_name] = float(temp_dict[key])
+                            obligative_zno += [key]
                     except KeyError:
                         if key in useless_zno : continue
                         else :
                             if 'Іноземна мова (' in key:
-                                coefs_dict.update({zno_dict[lang] : float(temp_dict[key]) for lang in key.split('(')[-1].split(')')[0].split(', ')})
-                                #optional_zno += key.split('(')[-1].split(')')[0].split(', ')
+                                splited_names = key.split('(')[-1].split(')')[0].split(', ')
+                                coefs_dict.update({zno_dict[lang] : float(temp_dict[key]) for lang in splited_names})
+                                extra_zno_list += splited_names
                             else: print (key)
-                #print(coefs_dict.keys())
-                
-                
-                print(obligative_zno)
-                print(optional_zno)
-                print(optional_zno_coef)             
-                
-                optional_zno = [zno_dict[i] for i in optional_zno]
-                coefs_dict['needed_zno'] = [zno_name for zno_name in list(coefs_dict.keys()) if zno_name not in fach_tvorch]
-                #print(coefs_dict)                
+
+                obligative_zno = [zno_dict[i] for i in obligative_zno]
+                extra_zno_list = [zno_dict[i] for i in extra_zno_list]
+                cleaned_oblidative_list = [i for i in obligative_zno if i not in fach_tvorch]
+                if len(extra_zno_list)==0: data_row['possible_combinations'] = [cleaned_oblidative_list]
+                else: data_row['possible_combinations'] = [cleaned_oblidative_list + [extra_zno] for extra_zno in extra_zno_list]
+
                 data_row['zno_coefs'] = coefs_dict
 
                 data_row['tvorchNeeded'] = False
@@ -173,9 +143,7 @@ for f in files:
                     data_row['isFreePlaces'] = True
                 else: data_row['isFreePlaces'] = False
 
-                if len(data_row['zno_coefs']['needed_zno']) == 0:
-                    data_row['searchable'] = False
-#                db.info.insert_one( data_row )
+                if data_row['point_mid_median']!=1000:
+                    db.info.insert_one( data_row )
                 counter -= 1
-                #print ('left %s'%counter)
-                
+                print ('left %s'%counter)
