17017f30806897fa7d49f2282066b6de4ad25535
Nikolai <prosto2307@gmail.com>
2017-02-09 15:31:47 +0200

    optimizing database scheme

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy import Column, Integer, String, JSON, Float
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,38 +10,40 @@ class City(Base):
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
-    city_name = Column(String)
-    city_link = Column(String)
+    name = Column(String)
+    link = Column(String)
+    university = relationship('University', back_populates='city')
 
     def __repr__(self):
-        return "<City(city_name='%s')" % self.city_name
+        return "<City(city_name='%s')>" % self.name
 
 
-class UniverInfo(Base):
-    __tablename__ = 'uni_info'
+class University(Base):
+    __tablename__ = 'universities'
 
     id = Column(Integer, primary_key=True)
     city_id = Column(Integer, ForeignKey('cities.id'))
-    uni_name = Column(String)
-    uni_type = Column(String)
+    name = Column(String)
+    type = Column(String)
     address = Column(String)
     phones = Column(String)
     website = Column(String)
     email = Column(String)
     link = Column(String)
 
-    city = relationship("City", back_populates="uni_info")
+    city = relationship("City", back_populates="university")
+    faculty = relationship("Faculty", back_populates="university")
 
     def __repr__(self):
-        return "<Univer(uni_name='%s', address='%s')>" % (
-            self.uni_name, self.address)
+        return "<University(uni_name='%s', link='%s')>" % (
+            self.name, self.link)
 
 
-class UniverData(Base):
-    __tablename__ = 'uni_speciality_data'
+class Faculty(Base):
+    __tablename__ = 'faculties'
 
     id = Column(Integer, primary_key=True)
-    uni_id = Column(Integer, ForeignKey('uni_info.id'))
+    uni_id = Column(Integer, ForeignKey('universities.id'))
     study_type = Column(String)
     free_places = Column(Integer)
     paid_places = Column(Integer)
@@ -49,38 +51,40 @@ class UniverData(Base):
     num_applied = Column(Integer)
     num_entered = Column(Integer)
     num_recommended = Column(Integer)
-    required_subj = Column(JSON)
+    required_subj = Column(String)
     degree = Column(String)
     degree_subname = Column(String)
     faculty = Column(String)
     specialization_1 = Column(String)
     specialization_2 = Column(String)
 
-    university_data = relationship("UniverInfo", back_populates="uni_speciality_data")
+    university = relationship("University", back_populates="faculty")
+    student = relationship("Student",
+                           back_populates="applied_to")
 
     def __repr__(self):
-        return "<UniverData(uni_id='%s', link='%s')>" % (
+        return "<Faculty(uni_id='%s', link='%s')>" % (
             self.uni_id, self.competition_link)
 
 
 class Student(Base):
-    __tablename__ = 'student_data'
+    __tablename__ = 'students'
 
     id = Column(Integer, primary_key=True)
-    uni_data_id = Column(Integer, ForeignKey('uni_speciality_data.id'))
-    student_name = Column(String)
+    uni_data_id = Column(Integer, ForeignKey('faculties.id'))
+    name = Column(String)
     priority = Column(Integer)
 
     school_certificate = Column(Float)
-    student_points = Column(Float)
-    student_zno = Column(Float)
+    points = Column(Float)
+    zno = Column(Float)
 
     entrance_points = Column(String)
     additional_points = Column(String)
     original_docs = Column(String)
 
-    student = relationship("UniverData", back_populates="student_data")
+    applied_to = relationship("Faculty", back_populates="student")
 
     def __repr__(self):
         return "<Student(student_name='%s', student_points='%s', original_docs='%s')>" % (
-            self.student_name, self.student_points, self.original_docs)
+            self.name, self.points, self.original_docs)
